cb4578b6bc3f9abad6e7e6b542079c90de321023
Sa3dawy <msadawy490@gmail.com>
2026-03-25 11:55:27 -0700

    22IS

--- index.js
+++ index.js
@@ -14,11 +14,17 @@ const SECRET = process.env.SECRET || "dalel_secret_key";
 app.use(cors());
 app.use(express.json());
 
+
 // 🔥 create uploads folder if not exists
 if (!fs.existsSync("uploads")) {
   fs.mkdirSync("uploads");
 }
 app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("API running 🔥");
+});
+
 
 
 // ✅ DB CONNECTION (FIXED)
