70ab15fabd45b0b1dbcfae1d3c97e6322bd7400f
Sa3dawy <msadawy490@gmail.com>
2026-03-25 12:19:33 -0700

    25is

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE, // تأكد من وجود الـ _ هنا إذا كانت موجودة في Variables
   port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
