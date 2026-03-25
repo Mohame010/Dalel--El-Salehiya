d0982c7d8ea2ef51f2b0e6bc20b544b750af8ae9
Sa3dawy <msadawy490@gmail.com>
2026-03-25 11:45:55 -0700

    21is

--- index.js
+++ index.js
@@ -41,9 +41,10 @@ db.getConnection((err, conn) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT + " 🔥");
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running 🔥");
 });
 
 
